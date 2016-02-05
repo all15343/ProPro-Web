0eb77932044fc473f2013e3d3f9cb65f749acf04
Alexander <all15343@spengergasse.at>
2016-02-05 13:35:27 +0100

    Enhanced deploy.php now you can switch branches

--- deploy.php
+++ deploy.php
@@ -1,5 +1,10 @@
 <?php
+	if(isset($_POST["branch"])){
+		shell_exec("git checkout ".$_POST["branch"]);
+	}
+
 	$commands = array(
+		'git branch',
 		'git fetch',
 		'git pull',
 		'git status',
@@ -17,17 +22,33 @@
 			$output .= "<br>";
 		}
 	}
+
+
+	$command = "git branch -r --no-color";
+	$brancharray = explode("\n",trim(shell_exec($command)));
 ?>
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html>
 <head>
 	<meta charset="UTF-8">
 	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
 <body style="background-color: #000000; color: #fafafa; font-weight: bold; padding: 0 10px;">
 <pre><br>
-<span style="font-size:40px; color:#FF9800;">ProPro - Deploy Service
-</span><br>
+<span style="font-size:50px; color:#FF9800;">ProPro - Deploy Service</span>
+<br>
+<span style="font-size:30px; color: #FF9800;">Branches:</span>
+<form method="post" action="deploy.php">
+<?php
+	foreach($brancharray AS $branch){
+		if(strpos($branch, '->') == false)
+			echo "<input type='submit' name='branch' value='" . substr(trim($branch),7) . "'/><br>";
+	}
+?>
+</form>
+<br>
+<span style="font-size:30px; color: #FF9800;">Log - Output:</span>
+<br>
 <?php echo $output; ?>
 </pre>
 </body>
